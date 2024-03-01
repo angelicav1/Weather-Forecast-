0d49a7446de39860b906c1e0a8a0eba0161a2d93
angelicav1 <114718142+angelicav1@users.noreply.github.com>
2024-02-29 20:49:40 -0800

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,14 +2,47 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector('#temperature');
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector('#city');
+  let descriptionElement = document.querySelector('#description');
+  let humdityElement = document.querySelector('#humidity');
+  let windSpeedElement = document.querySelector('#wind-speed');
+  let timeElement = document.querySelector('#time');
+  let date = new Date(response.data.time * 1000);
+
+  console.log(response.data.condition.description);
 
   cityElement.innerHTML = response.data.city;
+
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humdityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes},`;
 }
 
 function searchCity(city) {
   let apiKey = 'fct488b6f5355e5e50b3a1a472eo74dd';
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -23,4 +56,4 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener('submit', handleSearchSubmit);
 
-searchCity('Los Angeles');
+searchCity('Rancho Cucamonga');
