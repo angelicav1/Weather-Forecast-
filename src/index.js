d8b82a56ff54e24059e09e8b7ae36ea0b5a2ceb8
angelicav1 <114718142+angelicav1@users.noreply.github.com>
2024-03-01 21:06:31 -0800

    updated API Key and API Url

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = 'b2a5adcct04b33178913oc335f405433';
+  let apiKey = 'fct488b6f5355e5e50b3a1a472eo74dd';
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
@@ -55,7 +55,7 @@ function handleSearchSubmit(event) {
 }
 
 function getForecast(city) {
-  let apiKey = 'b2a5adcct04b33178913oc335f405433';
+  let apiKey = 'fct488b6f5355e5e50b3a1a472eo74dd';
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
 }
