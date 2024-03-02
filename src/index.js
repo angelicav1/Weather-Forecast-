609f0d6a6445d0aa8de8692272127d95db0356d8
angelicav1 <114718142+angelicav1@users.noreply.github.com>
2024-03-01 20:59:03 -0800

    updated index.js

--- src/index.js
+++ src/index.js
@@ -3,23 +3,21 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector('#city');
   let descriptionElement = document.querySelector('#description');
-  let humdityElement = document.querySelector('#humidity');
+  let humidityElement = document.querySelector('#humidity');
   let windSpeedElement = document.querySelector('#wind-speed');
   let timeElement = document.querySelector('#time');
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector('#icon');
 
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-
-  console.log(response.data.condition.description);
-
   cityElement.innerHTML = response.data.city;
-
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
-  humdityElement.innerHTML = `${response.data.temperature.humidity}`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed}`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -44,8 +42,8 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = 'fct488b6f5355e5e50b3a1a472eo74dd';
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiKey = 'b2a5adcct04b33178913oc335f405433';
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -56,7 +54,39 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function getForecast(city) {
+  let apiKey = 'b2a5adcct04b33178913oc335f405433';
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
+  let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  let forecastHtml = '';
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector('#forecast');
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener('submit', handleSearchSubmit);
-
 searchCity('Rancho Cucamonga');
