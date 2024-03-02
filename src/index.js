8a572fc1b9b3de5c4c9979a04cf1d6d72260f97c
angelicav1 <114718142+angelicav1@users.noreply.github.com>
2024-03-02 09:13:16 -0800

    updated index.js and style.css

--- src/index.js
+++ src/index.js
@@ -16,8 +16,6 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
-
-  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -43,7 +41,7 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = 'fct488b6f5355e5e50b3a1a472eo74dd';
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
@@ -54,42 +52,7 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function getForecast(city) {
-  let apiKey = 'fct488b6f5355e5e50b3a1a472eo74dd';
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-  axios(apiUrl).then(displayForecast);
-}
-
-function displayForecast(response) {
-  console.log(response.data);
-
-  let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  let forecastHtml = '';
-
-  response.data.daily.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
-      <div class="weather-forecast-day">
-        <div class="weather-forecast-date">Tuesday</div>
-        <div class="weather-forecast-icon">
-        <img src="${day.condition.icon_url}"</div>
-        <div class="weather-forecast-temperatures">
-          <div class="weather-forecast-temperature">
-            <strong>${Math.round(day.temperature.maximum)}°</strong>
-          </div>
-          <div class="weather-forecast-temperature">${Math.round(
-            day.temperature.minimum
-          )}°</div>
-        </div>
-      </div>
-    `;
-  });
-
-  let forecastElement = document.querySelector('#forecast');
-  forecastElement.innerHTML = forecastHtml;
-}
-
 let searchFormElement = document.querySelector('#search-form');
 searchFormElement.addEventListener('submit', handleSearchSubmit);
+
 searchCity('Rancho Cucamonga');
